45b8884ffd863dadbd7e3ebee8dafbe85d6d4586
Bohdan <bohdan.hukivskyi@outlook.com>
2019-06-22 13:41:42 +0300

    int d, h, Sum Rimana

--- Integral/Integral/Program.cs
+++ Integral/Integral/Program.cs
@@ -6,17 +6,29 @@ namespace Integral
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double from = 0;
+            double to = 3;
+            var integral = Integral(from, to);
+            Console.WriteLine(integral);
+            Console.ReadKey();
         }
 
         static double Integral(double from, double to)
         {
-            return 0;
+            double h = Math.Pow(10, -6);
+            int d = (int)((to - from) / h);
+            h = (to - from) / d;
+            double s = 0;
+            for (double x = from; x < to; x += h)
+            {
+                s += h * f(x);
+            }
+            return s;
         }
 
         static double f(double x)
         {
-            return 0;
+            return x*x;
         }
     }
 }
